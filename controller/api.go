d8a2c215bdeaecc3fca64eaed63bec7f4d8d078c
jianrong7 <ipwnjianrong@gmail.com>
2023-08-31 21:58:11 +0800

    feat: error handling for register

--- controller/api.go
+++ controller/api.go
@@ -15,8 +15,7 @@ type RegisterReq struct {
 	Teacher  string   `json:"teacher"`
 }
 
-// add error handling and test cases
-// handle invalid teacher or student use case
+// add test cases
 func Register(c *fiber.Ctx) error {
 	s := new(RegisterReq)
 	if err := c.BodyParser(s); err != nil {
@@ -24,14 +23,28 @@ func Register(c *fiber.Ctx) error {
 	}
 	teacherEmail := s.Teacher
 	var teacher model.Teacher
-	db.DB.Where("email = ?", teacherEmail).First(&teacher)
+	err := db.DB.Where("email = ?", teacherEmail).First(&teacher).Error
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "teacher not found"})
+	}
 
 	for _, studentEmail := range s.Students {
 		var student model.Student
-		db.DB.Where("email = ?", studentEmail).First(&student)
-		student.Teachers = append(student.Teachers, teacher)
+		err = db.DB.Where("email = ?", studentEmail).First(&student).Error
 
-		db.DB.Session(&gorm.Session{FullSaveAssociations: true}).Updates(&student)
+		if err != nil {
+			student = model.Student{
+				Email: studentEmail,
+				Teachers: []model.Teacher{
+					teacher,
+				},
+			}
+			db.DB.Create(&student)
+		} else {
+			student.Teachers = append(student.Teachers, teacher)
+			db.DB.Session(&gorm.Session{FullSaveAssociations: true}).Updates(&student)
+		}
+
 	}
 
 	return c.SendStatus(fiber.StatusNoContent)
@@ -49,7 +62,9 @@ func GetCommonStudents(c *fiber.Ctx) error {
 		FROM students
 		JOIN teacher_students ON students.id = teacher_students.student_id
 		JOIN teachers ON teacher_students.teacher_id = teachers.id
-		WHERE teachers.email in ?`, teacherEmails).Scan(&commonStudentEmails)
+		WHERE teachers.email in ?
+		AND students.deleted_at IS NULL
+		AND teachers.deleted_at IS NULL`, teacherEmails).Scan(&commonStudentEmails)
 
 	return c.JSON(fiber.Map{"students": commonStudentEmails})
 }
@@ -83,13 +98,18 @@ func RetrieveForNotifications(c *fiber.Ctx) error {
 	emailsFromNotification := utils.ExtractEmailsFromNotification(s.Notification)
 	db.DB.Raw(`SELECT DISTINCT students.email as studentEmail
 		FROM students
-		WHERE students.is_suspended = FALSE AND students.email IN ?`, emailsFromNotification).Scan(&validStudentsFromNotification)
+		WHERE students.is_suspended = FALSE
+		AND students.email IN ?
+		AND students.deleted_at IS NULL`, emailsFromNotification).Scan(&validStudentsFromNotification)
 
 	db.DB.Raw(`SELECT DISTINCT students.email as studentEmail
 		FROM students
 		JOIN teacher_students ON students.id = teacher_students.student_id
 		JOIN teachers ON teacher_students.teacher_id = teachers.id
-		WHERE students.is_suspended = FALSE AND (teachers.email = ? OR students.email IN ?)`, s.Teacher, emailsFromNotification).Scan(&registeredStudentEmails)
+		WHERE students.is_suspended = FALSE
+		AND (teachers.email = ? OR students.email IN ?)
+		AND students.deleted_at IS NULL
+		AND teachers.deleted_at IS NULL`, s.Teacher, emailsFromNotification).Scan(&registeredStudentEmails)
 
 	registeredStudentEmails = append(registeredStudentEmails, (validStudentsFromNotification)...)
 
