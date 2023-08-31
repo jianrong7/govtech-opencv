343162ca8dc969346ac5d61fc6c6f7b43ce6e3f6
jianrong7 <ipwnjianrong@gmail.com>
2023-08-31 18:01:30 +0800

    feat: add swagger docs

--- main.go
+++ main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-	"govtech-opencv/database"
+	"govtech-opencv/db"
 	"govtech-opencv/router"
 	"log"
+
+	_ "govtech-opencv/docs"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-
 func main() {
-		app := fiber.New(fiber.Config{
-			// Prefork:       true,
-			CaseSensitive: true,
-			ServerHeader:  "Fiber",
-			AppName: "govtech-opencv",
-		})
+	app := fiber.New(fiber.Config{
+		// Prefork:       true,
+		CaseSensitive: true,
+		ServerHeader:  "Fiber",
+		AppName:       "govtech-opencv",
+	})
 
-		app.Use(cors.New())
+	app.Use(cors.New())
+	// app.Get("/swagger/*", swagger.HandlerDefault)
 
-		database.ConnectToDB()
+	db.ConnectToDB()
 
-		router.SetupRoutes(app)
+	router.SetupRoutes(app)
 
-    log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":3000"))
 }
