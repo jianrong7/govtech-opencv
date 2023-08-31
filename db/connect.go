1be8f589ff74493d51910330e8e7996b155b3a0d
jianrong7 <ipwnjianrong@gmail.com>
2023-08-31 18:01:48 +0800

    chore: rename database package to db

---
+++ db/connect.go
@@ -0,0 +1,62 @@
+package db
+
+import (
+	"fmt"
+	"govtech-opencv/model"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var teacherEmails []string = []string{"teacherken@gmail.com", "teacherjoe@gmail.com"}
+var studentEmails []string = []string{"studentjon@gmail.com", "studenthon@gmail.com"}
+
+func seedTeachersTable() {
+	for _, email := range teacherEmails {
+		teacher := model.Teacher{Email: email}
+		DB.Create(&teacher)
+	}
+}
+
+func seedStudentsTable() {
+	for _, email := range studentEmails {
+		student := model.Student{Email: email, IsSuspended: false}
+		DB.Create(&student)
+	}
+}
+
+func ConnectToDB() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
+	var err error
+	dsn := os.Getenv("DB_URI")
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: newLogger})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	} else {
+		fmt.Println("Successfully connected to the database")
+	}
+
+	// remove after dev is done
+	// DB.Migrator().DropTable(&model.Student{}, &model.Teacher{})
+
+	DB.AutoMigrate(&model.Student{}, &model.Teacher{})
+	fmt.Println("Database migrated")
+
+	// remove after dev is done
+	// seedTeachersTable()
+	// seedStudentsTable()
+}
